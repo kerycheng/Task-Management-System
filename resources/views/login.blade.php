27d6fb4629359699d1b3dc13f0117adeb5a674d1
kerycheng <chengyankai1216@gmail.com>
2023-02-13 15:45:43 +0800

    fix login and register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,18 +7,24 @@
     <title>登入</title>
 </head>
 <body>
-    <h1>登入</h1>
-    <form action="/users/authenticate" method="POST">
-        @csrf
-        <div>
-            <label for="email">電子郵件：</label>
-            <input type="email" name="email" id="email" required>
-        </div>
-        <div>
-            <label for="password">密碼：</label>
-            <input type="password" name="password" id="password" required>
-        </div>
-        <button type="submit">登入</button>
-    </form>
+    <div>
+        <h1>登入</h1> 
+        <form action="/users/authenticate" method="POST">
+            @csrf
+            <div>
+                <label for="email">電子郵件：</label>
+                <input type="email" name="email" id="email" required>
+            </div>
+            <div>
+                <label for="password">密碼：</label>
+                <input type="password" name="password" id="password" required>
+            </div>
+            <button type="submit">登入</button>
+        </form>
+    </div>
+    <div>
+        <a href="/register">還沒有會員？點擊這裡進行註冊</a>
+    </div>
 </body>
+
 </html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -7,24 +7,26 @@
     <title>註冊</title>
 </head>
 <body>
-    <h1>註冊會員</h1>
-    <form action="/users" method="POST">
-        @csrf
-        <p>
-            <label for="name">名稱: </label>
-            <input type="text" name="name" id="name" required>
-        </p>
-        <p>
-            <label for="email">Email: </label>
-            <input type="email" name="email" id="email" required>
-        </p>
-        <p>
-            <label for="password">密碼: </label>
-            <input type="password" name="password" id="password" required>
-        </p>
-        <p>
-            <button type="submit">註冊</button>
-        </p>
-    </form>
+    <div>
+        <h1>註冊會員</h1>
+        <form action="/users" method="POST">
+            @csrf
+            <p>
+                <label for="name">名稱: </label>
+                <input type="text" name="name" id="name" required>
+            </p>
+            <p>
+                <label for="email">Email: </label>
+                <input type="email" name="email" id="email" required>
+            </p>
+            <p>
+                <label for="password">密碼: </label>
+                <input type="password" name="password" id="password" required>
+            </p>
+            <p>
+                <button type="submit">註冊</button>
+            </p>
+        </form>
+    </div>
 </body>
 </html>
